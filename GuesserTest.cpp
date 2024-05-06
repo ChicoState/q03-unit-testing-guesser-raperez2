01843909fda501fd3c6685c27aac7e3a31e378f3
raperez2 <70178601+raperez2@users.noreply.github.com>
2024-05-05 21:17:02 -0700

    Added Tests for Guesser Functions
    Implemented a variety of tests for each of the guesser functions except distance since it is private

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -20,3 +20,91 @@ TEST(GuesserTest, smoke_test)
     ASSERT_EQ( 1+1, 2 );
 }
 */
+
+TEST(GUESSERMatch, DirectMatch)
+{
+	Guesser tester("username");
+    EXPECT_TRUE(tester.match("username"));
+}
+
+TEST(GUESSERMatch, DirectFAIL)
+{
+	Guesser tester("username");
+    EXPECT_FALSE(tester.match("user"));
+}
+
+TEST(GUESSERemaining, TrueRemaining)
+{
+	Guesser tester("username");
+    EXPECT_EQ(tester.remaining(),3);
+}
+
+TEST(GUESSERMatch, LargeGuess)
+{
+	Guesser tester("username");
+    EXPECT_FALSE(tester.match("useristhepasswordthepasswordisuser"));
+}
+
+TEST(GUESSERMatch, SmallGuess)
+{
+	Guesser tester("username");
+    EXPECT_FALSE(tester.match("user"));
+}
+
+TEST(GUESSERMatch, MizedGuess)
+{
+	Guesser tester("username");
+    EXPECT_FALSE(tester.match("ubeRnOmy"));
+}
+
+TEST(GUESSERemaining, ThreeStrikes)
+{
+	Guesser tester("username");
+	tester.match("Username");
+	tester.match("usirname");
+	tester.match("usernime");
+    EXPECT_EQ(tester.remaining(),0);
+}
+
+TEST(GUESSERemaining, MaxMatch)
+{
+	Guesser tester("username");
+	tester.match("username");
+    EXPECT_EQ(tester.remaining(),3);
+}
+
+TEST(GUESSERemaining, FailPass)
+{
+	Guesser tester("username");
+	tester.match("userName");
+	tester.match("username");
+    EXPECT_EQ(tester.remaining(),3);
+}
+
+TEST(GUESSERemaining, BruteForce)
+{
+	Guesser tester("username");
+	tester.match("thisshouldbetherightpassword");
+	tester.match("username");
+    EXPECT_EQ(tester.remaining(),2);
+}
+
+TEST(GUESSERemaining, BruteForceRemaining)
+{
+	Guesser tester("username");
+	tester.match("thisshouldbetherightpassword");
+	tester.match("howaboutthistime");
+	tester.match("pleasework?");
+    EXPECT_EQ(tester.remaining(),2);
+}
+
+TEST(GUESSERMatch, PassFail)
+{
+	//Made under the assumption that a passed user can try and badly fail
+	Guesser tester("username");
+	tester.match("usirname");
+	tester.match("username");
+	tester.remaining();
+	tester.match("Itheusershouldwillinglyfail");
+    EXPECT_EQ(tester.remaining(),2);
+}
